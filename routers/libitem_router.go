c1ff996c8a7a989bdce952ca81adacf0e4a66465
追梦的人 <27672729+ouyangmeiwen@users.noreply.github.com>
2024-11-09 00:14:40 +0800

    123123
    123

--- routers/libitem_router.go
+++ routers/libitem_router.go
@@ -9,7 +9,7 @@ import (
 
 // RegisterOrderRoutes 注册订单路由
 func RegisterLibitemRoutes(r *gin.Engine) {
-	api := r.Group(global.OWEN_CONFIG.System.Pre + "/libitem").Use(middlewares.AuthMiddleware())
+	api := r.Group(global.OWEN_CONFIG.System.Pre + "/libitem").Use(middlewares.AuthMiddleware("libitem"))
 	{
 		api.POST("ImportExcel", ApiGroup.libItemApi.ImportExcel)
 	}
