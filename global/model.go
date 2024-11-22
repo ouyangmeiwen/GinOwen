68e4621f35ab321d9c8143d5288f1ef125495aaa
ouyangmeiwen <27672729+ouyangmeiwen@users.noreply.github.com>
2024-11-22 14:54:18 +0800

    123
    123

--- global/model.go
+++ global/model.go
@@ -23,9 +23,9 @@ type GVA_AuditedEntity struct {
 type GVA_FullAuditedEntity struct {
 	ID        uint           `gorm:"primarykey" json:"ID"` // 主键ID
 	CreatedAt time.Time      `json:"CreatedAt"`            // 创建时间
-	UpdatedAt time.Time      `json:"UpdatedAt"`            // 更新时间
-	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`       // 删除时间
 	CreatorID uint           `json:"CreatorID"`            // 创建者ID需要手动赋值
+	UpdatedAt time.Time      `json:"UpdatedAt"`            // 更新时间
 	UpdateID  uint           `json:"UpdateID"`             // 修改者ID需要手动赋值
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`       // 删除时间
 	DeleterID uint           `json:"DeleterID"`            // 删除者ID需要手动赋值
 }
