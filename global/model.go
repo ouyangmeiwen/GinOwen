65f06ce728acfbe4229bcd7c7bf2bb3aaeafb96e
ouyangmeiwen <27672729+ouyangmeiwen@users.noreply.github.com>
2024-11-22 14:53:46 +0800

    123
    123

--- global/model.go
+++ global/model.go
@@ -6,9 +6,26 @@ import (
 	"gorm.io/gorm"
 )
 
-type GVA_MODEL struct {
+type GVA_Entity struct {
+	ID        uint      `gorm:"primarykey" json:"ID"` // 主键ID
+	CreatedAt time.Time `json:"CreatedAt"`            // 创建时间
+	CreatorID uint      `json:"CreatorID"`            // 创建者ID需要手动赋值
+}
+
+type GVA_AuditedEntity struct {
+	ID        uint      `gorm:"primarykey" json:"ID"` // 主键ID
+	CreatedAt time.Time `json:"CreatedAt"`            // 创建时间
+	CreatorID uint      `json:"CreatorID"`            // 创建者ID需要手动赋值
+	UpdatedAt time.Time `json:"UpdatedAt"`            // 更新时间
+	UpdateID  uint      `json:"UpdateID"`             // 修改者ID需要手动赋值
+}
+
+type GVA_FullAuditedEntity struct {
 	ID        uint           `gorm:"primarykey" json:"ID"` // 主键ID
-	CreatedAt time.Time      // 创建时间
-	UpdatedAt time.Time      // 更新时间
-	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"` // 删除时间
+	CreatedAt time.Time      `json:"CreatedAt"`            // 创建时间
+	UpdatedAt time.Time      `json:"UpdatedAt"`            // 更新时间
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`       // 删除时间
+	CreatorID uint           `json:"CreatorID"`            // 创建者ID需要手动赋值
+	UpdateID  uint           `json:"UpdateID"`             // 修改者ID需要手动赋值
+	DeleterID uint           `json:"DeleterID"`            // 删除者ID需要手动赋值
 }
