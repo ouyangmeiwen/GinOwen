b39046b25c8fc3717df0ca8c4564b76067c1315b
追梦的人 <27672729+ouyangmeiwen@users.noreply.github.com>
2024-11-24 00:12:46 +0800

    123
    123

--- models/common.go
+++ models/common.go
@@ -1,4 +1,4 @@
-package global
+package models
 
 import (
 	"time"
@@ -6,13 +6,13 @@ import (
 	"gorm.io/gorm"
 )
 
-type GVA_Entity struct {
+type BaseEntity struct {
 	ID        uint      `gorm:"primarykey" json:"ID"` // 主键ID
 	CreatedAt time.Time `json:"CreatedAt"`            // 创建时间
 	CreatorID uint      `json:"CreatorID"`            // 创建者ID需要手动赋值
 }
 
-type GVA_AuditedEntity struct {
+type AuditedEntity struct {
 	ID        uint      `gorm:"primarykey" json:"ID"` // 主键ID
 	CreatedAt time.Time `json:"CreatedAt"`            // 创建时间
 	CreatorID uint      `json:"CreatorID"`            // 创建者ID需要手动赋值
@@ -20,7 +20,7 @@ type GVA_AuditedEntity struct {
 	UpdateID  uint      `json:"UpdateID"`             // 修改者ID需要手动赋值
 }
 
-type GVA_FullAuditedEntity struct {
+type FullAuditedEntity struct {
 	ID        uint           `gorm:"primarykey" json:"ID"` // 主键ID
 	CreatedAt time.Time      `json:"CreatedAt"`            // 创建时间
 	CreatorID uint           `json:"CreatorID"`            // 创建者ID需要手动赋值
