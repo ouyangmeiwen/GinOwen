fa6faf7afe71e2ec72723e29264729c17ca1dccf
追梦的人 <27672729+ouyangmeiwen@users.noreply.github.com>
2024-11-24 21:59:08 +0800

    123
    123

--- utils/jsonutil.go
+++ utils/jsonutil.go
@@ -17,6 +17,18 @@ func FromJSON(data string, v interface{}) error {
 	return json.Unmarshal([]byte(data), v)
 }
 
+func ToRawMessage(v interface{}) (json.RawMessage, error) {
+	bodyBytes, err := json.Marshal(v)
+	if err != nil {
+		return nil, err
+	}
+	return json.RawMessage(bodyBytes), err
+}
+
+func StringToRawMessage(s string) json.RawMessage {
+	return json.RawMessage(s)
+}
+
 func PrettyPrint(v interface{}) {
 	bytes, _ := json.MarshalIndent(v, "", "  ")
 	fmt.Println(string(bytes))
