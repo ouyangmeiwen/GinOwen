b73bcb40890b2690c2701ae009b79dcc5647f526
ouyangmeiwen <27672729+ouyangmeiwen@users.noreply.github.com>
2024-12-03 09:31:06 +0800

    1
    1

--- serviceinit/interface.go
+++ serviceinit/interface.go
@@ -1,31 +1,33 @@
 package serviceinit
 
-import "fmt"
+import (
+	"log"
+)
 
 type AutoService struct{}
 
 // 定义接口
-type Migratable interface {
+type Migratable_interface interface {
 	CusAutoMigrate()
 }
-type SysData interface {
+type SysData_interface interface {
 	CusSyncDatabase() error
 }
 
 // 判断并调用
 func callCusAutoMigrateIfExists(service interface{}) {
-	if m, ok := service.(Migratable); ok {
+	if m, ok := service.(Migratable_interface); ok {
 		m.CusAutoMigrate() // 调用方法
 	} else {
-		fmt.Println("CusAutoMigrate method does not exist")
+		log.Fatalf("CusAutoMigrate method does not exist")
 	}
 }
 
 // 判断并调用
 func callCusSyncDatabaseIfExists(service interface{}) {
-	if m, ok := service.(SysData); ok {
+	if m, ok := service.(SysData_interface); ok {
 		m.CusSyncDatabase() // 调用方法
 	} else {
-		fmt.Println("CusAutoMigrate method does not exist")
+		log.Fatalf("CusAutoMigrate method does not exist")
 	}
 }
